db61226cb52b46b22eba2b5a482fa55491a4428c
mjabarin1 <maisjab101@gmail.com>
2026-08-12 15:55:10 +0300

    bigger cell size and smaller maze

--- maze_visualizer.py
+++ maze_visualizer.py
@@ -2,7 +2,7 @@ import pygame
 from mazegenerator import MazeGenerator
 
 def display_maze(surface,our_seed):
-    maze = MazeGenerator(seed=our_seed,size=(46,20))
+    maze = MazeGenerator(seed=our_seed,size=(30,14))
     grid = maze.maze
     #pygame.init()
     #main_surface = pygame.display.set_mode((1920,1080))
@@ -16,7 +16,7 @@ def display_maze(surface,our_seed):
             exit()
     height = 0
     for row in grid:
-        x = 40
+        x = 60 #cell width
         width = 0
         for cell in row:
             if cell == 15 :
@@ -24,14 +24,14 @@ def display_maze(surface,our_seed):
             else:
                 color = (0, 0, 255)
             if cell & 8: #w
-                pygame.draw.line(main_surface, color, (x,y), (x, y+40))
+                pygame.draw.line(main_surface, color, (x,y), (x, y+60))
             if cell & 1: #n
-                pygame.draw.line(main_surface, color, (x,y), (x+40, y))
+                pygame.draw.line(main_surface, color, (x,y), (x+60, y))
             if width == maze._width -1 :
-                pygame.draw.line(main_surface, color, (x+40,y), (x+40, y+40))
+                pygame.draw.line(main_surface, color, (x+60,y), (x+60, y+60))
             if height ==maze._height - 1:
-                pygame.draw.line(main_surface, color, (x,y+40), (x+40, y+40))
-            x+=40
+                pygame.draw.line(main_surface, color, (x,y+60), (x+60, y+60))
+            x+=60
             width += 1
         height += 1
-        y+=40
+        y+=60#cell height
